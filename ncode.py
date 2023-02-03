4497543fb4de80854cca187c569948b562b657af
learngitn <123749532+learngitn@users.noreply.github.com>
2023-02-03 23:00:05 +0530

    new file for practice

---
+++ ncode.py
@@ -0,0 +1,16 @@
+country = India
+
+mkdir India
+cd India
+
+vi Banglore mumbai dehali chennai
+
+cat Banglore
+echo "karnataka capital"
+cat mumbai
+echo "maharashtra capital"
+cat delhi
+echo "India capital"
+cta chennai
+echo "tamilunadu capital"
+
